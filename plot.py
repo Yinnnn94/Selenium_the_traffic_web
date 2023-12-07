609b9245a4dcfff2509c627492540282fac8237f
Yinnnn94 <rungina890904@gmail.com>
2023-12-07 20:14:51 +0800

    Main program of plot

--- plot.py
+++ plot.py
@@ -2,12 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv(r'concat_data.csv', encoding = 'Big5')
-age = set(data['Age'])
+age = list(set(data['Age']))
+vehicle_type = list(set(data['Vehicle_type']))
+
 for i in age:
-    data_age = data[data['Age'] == i]
-    plt.figure()
-    plt.title(i)
-    plt.plot(data_age['Year'], data_age['Casualties'], label = 'Casualties')
-    plt.plot(data_age['Year'], data_age['Death_toll'], label = 'Death_toll')
-    plt.plot(data_age['Year'], data_age['Injuries'], label = 'Injuries')
-    plt.show()
+    if '不明' in i :
+        pass
+    else:
+        fig, ax = plt.subplots(1, 2)
+        for v, plot_number in zip(vehicle_type, ax):
+            filter1 = data['Age'] == i
+            filter2 = data['Vehicle_type'] == v
+            filter_data = data[filter1 & filter2]
+            plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
+            plot_number.set_title(f'{i}{v}死亡與受傷率')
+            plot_number.plot(filter_data['Year'], filter_data['death_rate'], label = f'{v}死亡率')
+            plot_number.legend()
+        plt.show()
